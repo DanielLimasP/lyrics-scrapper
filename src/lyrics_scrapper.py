f66744483481ea8a3cb9d97518286e342b3c1e86
Daniel Limas <amedlimas97@gmail.com>
2020-12-06 19:38:20 -0700

    Added saving func

--- src/lyrics_scrapper.py
+++ src/lyrics_scrapper.py
@@ -4,11 +4,17 @@ import requests
 from pathlib import Path
 import os
 
+# ANSI COLORS:
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+CYAN = "\033[0;36m"
+LIGHT_GRAY = "\033[0;37m"
+
 class Lyrics_Scrapper:
     def __init__(self):
         super().__init__()
         # It's kinda redundant to have both the url and the res together
-        self.url = 'https://www.azlyrics.com/lyrics/metallica/theshorteststraw.html'
+        self.url = 'https://www.azlyrics.com/lyrics/strokes/undercontrol.html'
         self.res = requests.get(self.url)
         self.soup = bs4.BeautifulSoup(self.res.text, 'html.parser')
 
@@ -16,7 +22,7 @@ class Lyrics_Scrapper:
         try:
             self.res.raise_for_status()
         except Exception as ex:
-            print('There was a problem: {}'.format(ex))
+            print(RED + 'There was a problem: {}'.format(ex)+ LIGHT_GRAY)
 
     # It really gets the main content of the page
     def get_song_lyrics(self):
@@ -29,6 +35,14 @@ class Lyrics_Scrapper:
         '''
         content = self.get_soup_content('.main-page')
         artist, song, album = self.get_song_info()
+
+        print('Dowloading lyrics...')
+        print(CYAN + '''
+        artist: {}
+        album: {}
+        song: {}
+        '''.format(artist, album, song)+ LIGHT_GRAY)
+
         # Need to get rid of the " " and replace spaces with _
         song = song[1:-1]
         song = song.replace(' ', '_')
@@ -39,8 +53,8 @@ class Lyrics_Scrapper:
 
         song = song + '.html'
 
-        self.save_content(content, song, artist, album)
-        
+        print(self.save_content(content, song, artist, album))
+
     def get_song_info(self):
         '''
         :type attr: None
@@ -71,29 +85,48 @@ class Lyrics_Scrapper:
         soup_content = self.soup.select(attr)
         return soup_content
 
+    # TODO: Fix the problem with special chars when saving
     def save_content(self, content, song, artist, album):
+        '''
+        :type attr: str, str, str
+        :rtype: str
+        
+        save_content: 
+        Saves the requested song lyrics
+        '''
         cwd = Path.cwd()
         # Save the file
         try:
-            if os.path.exists(cwd / artist / album):
-                new_file = open(artist / album / song, 'w+', encoding='utf-8')
+            path_exists = os.path.exists('Lyrics'+ '/' +artist+ '/' +album)
+            if path_exists: 
+                new_file = open('Lyrics'+ '/' +artist+ '/' +album+ '/' +song, 'w+', encoding='utf-8')
                 new_file.write(str(content))
                 new_file.close()
-                print('File saved')
-                return 'File saved'
-            else: 
+                return GREEN +'File saved'+ LIGHT_GRAY
+            else:
                 # Create the new dir for the album
-                Path.mkdir(cwd / artist / album)
-                new_file = open(artist / album / song, 'w+', encoding='utf-8')
+                path_exists = os.path.exists('Lyrics' + '/' +artist)
+                if path_exists:
+                    Path('Lyrics'+ '/' +artist+ '/' + album).mkdir()
+                else:
+                    Path('Lyrics'+ '/' +artist).mkdir()
+                    Path('Lyrics'+ '/' +artist+ '/' + album).mkdir()
+                new_file = open('Lyrics'+ '/' +artist+ '/' +album+ '/' +song, 'w+', encoding='utf-8')
                 new_file.write(str(content))
                 new_file.close()
-                print('File saved')
                 return 'File saved'
         except Exception as ex:
-            print('There was a problem: {}'.format(ex))
-            return 'There was a problem: {}'.format(ex)
+            return RED +'There was a problem: {}'.format(ex)+ LIGHT_GRAY
+            
 
     def set_url(self, url):
+        '''
+        :type attr: str
+        :rtype: None
+        
+        set_url: 
+        setter for the url and req
+        '''
         self.url = url
         self.res = requests.get(self.url)
 
